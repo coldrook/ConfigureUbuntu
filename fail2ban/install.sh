dd0c6d5185cb03f2636934282d9b9d49b3392edd
coldrook <8768561+coldrook@users.noreply.github.com>
2024-11-29 23:24:11 +0800

    Update install.sh

--- fail2ban/install.sh
+++ fail2ban/install.sh
@@ -1,8 +1,8 @@
 # Installs fail2ban on the latest version of ubuntu
 #fixes the broken dependecy issue in apt-get install fail2ban 
 
-wget -O fail2ban.deb https://github.com/fail2ban/fail2ban/releases/download/1.1.0/fail2ban_1.1.0-1.upstream1_all.deb
-wget -O fail2ban.deb.asc https://github.com/fail2ban/fail2ban/releases/download/1.1.0/fail2ban_1.1.0-1.upstream1_all.deb.asc
+wget -N --no-check-certificate https://github.com/fail2ban/fail2ban/releases/download/1.1.0/fail2ban_1.1.0-1.upstream1_all.deb
+wget -N --no-check-certificate https://github.com/fail2ban/fail2ban/releases/download/1.1.0/fail2ban_1.1.0-1.upstream1_all.deb.asc
 
 gpg --verify fail2ban.deb.asc fail2ban.deb
 dpkg -I fail2ban.deb
@@ -12,3 +12,4 @@ sudo apt -f install
 
 sudo systemctl start fail2ban
 sudo systemctl status fail2ban
+
