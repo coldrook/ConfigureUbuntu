f4fa3a50592a26e2be49a34d7cbce4406e062589
coldrook <8768561+coldrook@users.noreply.github.com>
2024-11-30 00:33:16 +0800

    Update install.sh

--- fail2ban/install.sh
+++ fail2ban/install.sh
@@ -1,7 +1,8 @@
 # Installs fail2ban on the latest version of ubuntu
 #fixes the broken dependecy issue in apt-get install fail2ban 
 
-wget --no-check-certificate -O fail2ban.deb https://github.com/fail2ban/fail2ban/releases/download/1.1.0/fail2ban_1.1.0-1.upstream1_all.deb 
+wget --no-check-certificate -O fail2ban.deb https://github.com/fail2ban/fail2ban/releases/download/1.1.0/fail2ban_1.1.0-1.upstream1_all.deb
+ 
 wget --no-check-certificate -O fail2ban.deb.asc https://github.com/fail2ban/fail2ban/releases/download/1.1.0/fail2ban_1.1.0-1.upstream1_all.deb.asc
 
 gpg --verify fail2ban.deb.asc fail2ban.deb
